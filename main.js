2fd5c2454eeb3343377cbbf7dbdbc4d8607c51c2
fugu133 <fugu0220@gmail.com>
2023-03-17 21:48:24 +0900

    first commitcommit

---
+++ main.js
@@ -0,0 +1,12 @@
+
+"use strict";
+Cesium.Ion.defaultAccessToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI4NmY1MzU3MC04OGM4LTQ0YzctOWNjNC03YzIwOTlhODQ2ZDgiLCJpZCI6MTI5MTU3LCJpYXQiOjE2NzkwMzg4NDB9.I18hj604p8pbNdg73hVjDUqpzyf5jzrYVkXleEi4Pp4";
+var viewer = new Cesium.Viewer("cesium", {
+   shouldAnimate: true,
+});
+
+viewer.dataSources.add(
+   Cesium.CzmlDataSource.load("koyoh_orbit.json")
+);
+
+viewer.camera.flyHome(0);
